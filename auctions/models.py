1ae080e7c1fc6969175caf954137329d0582353a
Alek Yoanda <alexyoandatampu1@gmail.com>
2022-07-12 21:39:08 +0700

    Updating models

--- auctions/models.py
+++ auctions/models.py
@@ -22,10 +22,15 @@ class AuctionListing(models.Model):
     date_created = models.DateTimeField(default=datetime.now())
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category", null=True, blank=True)
 
+    def __str__(self) -> str:
+        return self.title
+    
+
 class Bid(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     bid_amount = models.FloatField()
     time_updated = models.DateTimeField(auto_now=True)
+    listing_item = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="listing_item", default=AuctionListing.objects.first().pk)
 
     @classmethod
     def highest_bid(self):
@@ -34,3 +39,6 @@ class Bid(models.Model):
             if bid.bid_amount > highest_bid:
                 highest_bid = bid.bid_amount
         return highest_bid
+
+    def __str__(self) -> str:
+        return f"Bid ({self.user.username} | {self.listing_item.title}) "
